9bc834431df9aebb89ecee288598cf2bdbaf1224
mabdelhack <m.abouelyazid@gmail.com>
2020-05-23 00:24:55 -0500

    move right and left

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
+  const startPosition = 4
+  const speedInterval = 300
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -43,11 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 
-  let currentPosition = 6
+  let currentPosition = startPosition
 
   let randomTetromino = Math.floor(Math.random() * theTetrominos.length)
-  let randomPosition = Math.floor(Math.random() * theTetrominos[0].length)
-  let current = theTetrominos[randomTetromino][randomPosition]
+  let randomRotation = Math.floor(Math.random() * theTetrominos[0].length)
+  let currentRotation = randomRotation
+  let current = theTetrominos[randomTetromino][currentRotation]
 
 
   function draw() {
@@ -62,5 +65,77 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  draw()
+  timerID = setInterval(moveDown, speedInterval)
+
+  function control (e) {
+    if (e.keyCode === 37) {
+      moveLeft()
+    }
+    else if (e.keyCode === 39) {
+      moveRight()
+    }
+    else if (e.keyCode === 38) {
+      rotate()
+    }
+    else if (e.keyCode === 39) {
+    //  moveDown()
+    }
+  }
+
+  document.addEventListener('keyup', control)
+
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      randomTetromino = Math.floor(Math.random() * theTetrominos.length)
+      randomRotation = Math.floor(Math.random() * theTetrominos[0].length)
+      current = theTetrominos[randomTetromino][randomRotation]
+      currentPosition = startPosition
+      draw()
+    }
+  }
+
+
+  function moveLeft () {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if (!isAtLeftEdge) currentPosition -= 1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1
+    }
+    draw()
+  }
+
+  function moveRight () {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+    if (!isAtRightEdge) currentPosition += 1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1
+    }
+    draw()
+  }
+
+  function rotate () {
+    undraw()
+    currentRotation++
+    if (currentRotation === current.length) {
+      currentRotation = 0
+    }
+    current = theTetrominos[randomTetromino][currentRotation]
+    draw()
+  }
+
+
 })
